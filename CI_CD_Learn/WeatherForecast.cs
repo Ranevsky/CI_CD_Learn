f393ac739bdaf200a7a47c4808eb1038cd855a96
Ranevsky <ranevsky91@mail.ru>
2022-10-30 17:57:02 +0300

    init

---
+++ CI_CD_Learn/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace CI_CD_Learn;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
